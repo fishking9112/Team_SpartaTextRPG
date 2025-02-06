d8fe45574c9781e9b3c88fdeecdfba3098cced62
RRoroan <tjdals8788@gmail.com>
2025-02-06 20:44:33 +0900

    [feat] ShopScene > ShowShop add options

--- Team_SpartaTextRPG/Scene/ShopScene.cs
+++ Team_SpartaTextRPG/Scene/ShopScene.cs
@@ -10,8 +10,26 @@ namespace Team_SpartaTextRPG
     {
         public void ShowShop ()
         {
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
             Console.WriteLine("0. 돌아가기");
             SceneManager.instance.Menu(ShowShop, TownScene.instance.Game_Main);
+        }
+
+        public void selectMenu (int _input)
+        {
+            if (_input == 0)
+            {
+
+            }
+            else if (_input == 1)
+            {
+
+            }
+            else if ( _input == 2)
+            {
+
+            }
         }
     }
 }
