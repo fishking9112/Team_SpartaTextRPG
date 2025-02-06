ed0401b31c05085bdeac9ac32bc3d274322adde5
fishking9112 <fishking9112@gmail.com>
2025-02-06 17:27:01 +0900

    [ feat ] 클래스 계층 구조 생성

---
+++ Team_SpartaTextRPG/Scene/Scenes/Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_SpartaTextRPG
+    internal class Status : Scene
+{
+    }
+}
