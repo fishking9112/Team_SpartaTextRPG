aa308b59253348be0fb0aa77ffdee781a709f9b1
박진우 <tree6316mte@gmail.com>
2025-02-06 17:03:51 +0900

    [ Feat ] 초기 작업에 필요한 Manager 파일 생성

---
+++ Team_SpartaTextRPG/Helper/Singleton.cs
@@ -0,0 +1,21 @@
+namespace Team_SpartaTextRPG.Helper
+{
+    public class Singleton<T> where T : class, new()
+    {
+        private static T? m_instance;
+
+        // Singleton 인스턴스에 접근하는 속성
+        public static T instance
+        {
+            get
+            {
+                // Double-Check Locking
+                if (m_instance == null)
+                {
+                    m_instance = new T();
+                }
+                return m_instance;
+            }
+        }
+    }
+}
