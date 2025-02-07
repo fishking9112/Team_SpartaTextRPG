a2a0a8fac0f46efc2468fdadd4f9666cac81082b
100ZADA <5528asd@gmail.com>
2025-02-07 14:25:10 +0900

    Add 던전에 몬스터 소환
    Stage 입장 시 몬스터 소환 코드 작성

--- Team_SpartaTextRPG/Scene/DungeonScene.cs
+++ Team_SpartaTextRPG/Scene/DungeonScene.cs
@@ -11,12 +11,15 @@ namespace Team_SpartaTextRPG
 {
     internal class DungeonScene : Helper.Singleton<DungeonScene>
     {
+        Monster[] monster = new Monster[3];
+
         public void Dungeon_Title()
         {
             Console.WriteLine("1. [ Stage 1 ]");
             //Console.WriteLine("2. [ Stage 2 ]");
             //Console.WriteLine("3. [ Stage 3 ]");
             Console.WriteLine("0. [ 돌아가기 ]");
+            initStage();
 
             SceneManager.instance.Menu(Dungeon_Title, TownScene.instance.Game_Main, () => Select_Stage(1));
         }
@@ -26,7 +29,7 @@ namespace Team_SpartaTextRPG
             if (_index == 1)
             {
                 // 던전 진입시 몬스터 소환
-                initStage();
+                DrawMonster_Info();
             }
 
         }
@@ -37,15 +40,17 @@ namespace Team_SpartaTextRPG
         public void initStage()
         {
             // 몬스터 정보
-            Monster Monster_A = new Monster("슬라임", 1, 10, 1, 1);
-            Monster Monster_B = new Monster("고블린", 3, 18, 3, 5);
-            Monster Monster_C = new Monster("오크", 5, 25, 5, 7);
+            monster[0] = new Monster("슬라임", 1, 5, 5);
+            monster[1] = new Monster("고블린", 3, 10, 10);
+            monster[2] = new Monster("오크", 5, 20, 20);
         }
 
         // 현재 몬스터를 Select_Stage로 출력
         public void DrawMonster_Info()
         {
-
+            monster[0].Monster_Info();
+            monster[1].Monster_Info();
+            monster[2].Monster_Info();
         }
     }
 }
