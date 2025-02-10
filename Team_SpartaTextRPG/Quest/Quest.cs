0fad1a24f7f807f62eaeb85aa2cfb0e1e3433803
fishking9112 <fishking9112@gmail.com>
2025-02-10 21:12:46 +0900

    [ Feat ] Quest 관련 클래스 추가
    퀘스트보드 , 퀘스트 클래스 추가

---
+++ Team_SpartaTextRPG/Quest/Quest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team_SpartaTextRPG
+{
+    enum QUEST_TYPE { Item_Collection , Kill_Count }
+    internal class Quest
+    {
+        public Quest(string _name , string _Des , string _Target , int _Count , int _Price) 
+        {
+            Name = _name;
+            Description = _Des;
+            TargetName = _Target;
+            MaxCount = _Count;
+            Price = _Price;
+        }
+
+        //퀘스트에 필요한 것 ?
+        // 퀘스트 제목
+        public string Name { get; set; }
+        // 퀘스트 내용
+        public string Description { get; set; }
+        // 목표 이름
+        public string TargetName { get; set; }
+        // 현재 갯수
+        public int CurCount {  get; set; }
+        // 목표 갯수
+        public int MaxCount { get; set; }
+        // 보상
+        public int Price { get; set; }
+        // 퀘스트 타입 ( 아이템 수집 or 킬 미션 )
+        // 고민중임
+    }
+}
--- Team_SpartaTextRPG/Scene/QuestBoardScene.cs
+++ Team_SpartaTextRPG/Scene/QuestBoardScene.cs
@@ -8,5 +8,31 @@ namespace Team_SpartaTextRPG
 {
     internal class QuestBoardScene : Helper.Singleton<QuestBoardScene>
     {
+        void InitQuestList()
+        {
+
+        }
+        void ShowMenu()
+        {
+            Console.WriteLine("1. 퀘스트 받기");
+            Console.WriteLine("2. 퀘스트 완료");
+            Console.WriteLine("0. 돌아가기");
+            SceneManager.instance.Menu(ShowMenu, TownScene.instance.Game_Main, Quest_Board, Quest_Complete);
+        }
+
+
+        void Quest_Board()
+        {
+            Console.WriteLine("퀘스트 목록");
+            Console.WriteLine("============");
+            Console.WriteLine();
+
+
+        }
+        void Quest_Complete()
+        {
+
+        }
+
     }
 }
