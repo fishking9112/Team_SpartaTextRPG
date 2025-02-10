f609e152e2e1d4671c71e23dc6f76e6146be3170
박진우 <tree6316mte@gmail.com>
2025-02-10 16:57:35 +0900

    [Feat] 휴식하기 그래픽

--- Team_SpartaTextRPG/Scene/RestScene.cs
+++ Team_SpartaTextRPG/Scene/RestScene.cs
@@ -11,39 +11,43 @@ namespace Team_SpartaTextRPG
         Player player = GameManager.instance.player;
         public void Show_Rest()
         {
-            Console.Clear();
-            Utill.ColorWriteLine("휴식하기");
-            Console.WriteLine($"500 G를 내면 체력을 회복할 수 있습니다. (보유골드 : {player.Gold} G)");
-            Console.WriteLine();
-            Console.WriteLine("1. 휴식하기");
-            Console.WriteLine();
-            Console.WriteLine("0. 나가기");
-            Console.WriteLine();
+            TitleManager.instance.WriteTitle("여관", ConsoleColor.Yellow);
 
-            //RestMenu로 가기
-            SceneManager.instance.Menu(Show_Rest, TownScene.instance.Game_Main, RestMenu);
+            StringBuilder sb = new();
+            sb.AppendLine($"500 G를 내면 체력을 회복할 수 있습니다. (보유골드 : {player.Gold} G)");
+
+            ScreenManager.instance.AsyncText(sb);
+
+            InputKeyManager.instance.ArtMenu(
+                ($"휴식하기", $"500 G를 내면 체력을 회복할 수 있습니다. (보유골드 : {player.Gold} G)", () => RestMenu()), 
+                ($"나가기", "마을로 나갑니다.", () => {TownScene.instance.Game_Main(); }));
         }
         
         public void RestMenu()
         {
-                if ( player.MaxHP== player.HP)             //플레이어 체력이 최대치일때
-                {
-                    Console.WriteLine("체력이 이미 최대치입니다.\n\n");
-                }
-                else if ( player.Gold < 500)               //플레이어 골드가 500미만일때
-                {
-                    Console.WriteLine("Gold가 부족합니다.\n\n");
-                }
-                else
-                {
-                    player.Gold -= 500;                    //플레이어 골드가 -500
-                    player.HP = player.MaxHP;              //플레이어 체력이 최대치가됨
-                    Console.WriteLine("휴식을 완료했습니다.\n\n");
-                }
-            Console.WriteLine("0.확인");
-            Console.WriteLine();
-            //RestMenu 에서 Show_Rest로 돌아가기
-            SceneManager.instance.Menu(RestMenu, Show_Rest);
+            TitleManager.instance.WriteTitle("휴식하기", ConsoleColor.Yellow);
+
+            
+            StringBuilder sb = new();
+            if ( player.MaxHP== player.HP)             //플레이어 체력이 최대치일때
+            {
+                sb.AppendLine("체력이 이미 최대치입니다.\n\n");
+            }
+            else if ( player.Gold < 500)               //플레이어 골드가 500미만일때
+            {
+                sb.AppendLine("Gold가 부족합니다.\n\n");
+            }
+            else
+            {
+                player.Gold -= 500;                    //플레이어 골드가 -500
+                player.HP = player.MaxHP;              //플레이어 체력이 최대치가됨
+                sb.AppendLine("휴식을 완료했습니다.\n\n");
+            }
+
+            ScreenManager.instance.AsyncText(sb);
+
+            InputKeyManager.instance.ArtMenu(
+                ($"확인", "휴식하기로 돌아갑니다.", () => Show_Rest()));
 
         }
     }
