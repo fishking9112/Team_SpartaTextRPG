44b83cae3cdeececb76bc01d80d3e76066c72199
박진우 <tree6316mte@gmail.com>
2025-02-12 18:38:59 +0900

    [ Fix ] EndingScene 끝나는 지점 수정

--- Team_SpartaTextRPG/Scene/EndingScene.cs
+++ Team_SpartaTextRPG/Scene/EndingScene.cs
@@ -14,7 +14,7 @@ namespace Team_SpartaTextRPG
             
             ScreenManager.instance.AsyncVideo("./resources/ending.gif", _isContinue: false, _isReversal: true, _frame: 66);
 
-            Utill.Sleep(66*320);
+            Utill.Sleep(66*200);
 
             InputKeyManager.instance.ArtMenu(("게임 종료", "플레이 해주셔서 감사합니다!", StartScene.instance.Game_Quit));
         }
